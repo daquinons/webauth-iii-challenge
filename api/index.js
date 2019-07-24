aa0bac5b34ffa399c630b37dfe39a76d4cb6423c
David Quiñones <daquinons@users.noreply.github.com>
2019-07-24 13:49:19 +0200

    Create endpoints to register and login

--- api/index.js
+++ api/index.js
@@ -3,9 +3,16 @@ const usersRouter = require('./users/router');
 
 const router = express.Router();
 
-
 router.get('/api', (req, res) => {
   res.json({ message: 'API is up' });
+});
+
+router.post('/api/register', (req, res) => {
+  res.json({ message: 'Post register' });
+});
+
+router.post('/api/login', (req, res) => {
+  res.json({ message: 'Post login' });
 });
 
 router.use('/api/users', usersRouter);
